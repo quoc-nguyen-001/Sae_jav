19a491dbe78ea1b599638416249c124e68f7389b
quoc-nguyen-001 <nguyen.quocdat.1029@gmail.com>
2025-05-11 19:38:47 +0200

    Update Djikstra.java

--- Djikstra.java
+++ Djikstra.java
@@ -26,7 +26,7 @@ public class Dijkstra<T> implements ShortestPath<T> {
 
 		
 		dist.put(src, 0);
-        	pred.put(src, null);
+        pred.put(src, null);
         
 		T sommetActuel = src;
 		sommets_reconnus.add(sommetActuel);
@@ -45,20 +45,20 @@ public class Dijkstra<T> implements ShortestPath<T> {
 			
 			for(T sommet : sommets_reconnus) { //voir tous les arcs des sommets deja reconnus
 				arcs = g.getSucc(sommet);
-				
-				for(Arc<T> arc : arcs) { // voir tous ses arcs
-					if(arc.val() < 0) {
-						throw new IllegalArgumentException("Arc négatif détecté");
-					}
-					
-					if(!sommet_existant(sommets_reconnus, arc.dst())) {// si le sommet est deja reconnu alors on sen fiche l'arc pour eviter des boucles
-						if(poids_arc > arc.val() + dist.get(sommet)) {
+				if(arcs != null) {
+					for(Arc<T> arc : arcs) { // voir tous ses arcs
+						if(arc.val() < 0) {
+							throw new IllegalArgumentException("Arc négatif détecté");
+						}
+						
+						if(!sommets_reconnus.contains(arc.dst()) && poids_arc > arc.val() + dist.get(sommet) ) {// si le sommet est deja reconnu alors on sen fiche l'arc pour eviter des boucles
 							poids_arc= arc.val() + dist.get(sommet);
 							sommetDestination= arc.dst();
 							son_pred = sommet ;
 						}
 					}
 				}
+				
 			}
 			if(sommetDestination == sommetActuel ) {
 				return new Distances<>(dist,pred);
@@ -68,21 +68,12 @@ public class Dijkstra<T> implements ShortestPath<T> {
 			sommets_reconnus.add(sommetActuel);
 			
 			dist.put(sommetActuel, poids_arc);
-	       		pred.put(sommetActuel, son_pred);
+	        pred.put(sommetActuel, son_pred);
 			
 			
 		}
 		
 		
 		
-	}
-	
-	private boolean sommet_existant(List<T> sommets_reconnus, T sommet) {
-		for(T sommet_1 : sommets_reconnus) {
-			if(sommet == sommet_1) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
